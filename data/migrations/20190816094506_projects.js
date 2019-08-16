4d46ad2fce1dbf428b7483a70642a92cc93313dc
Clark Williams <theclarkwilliams@gmail.com>
2019-08-16 10:01:37 -0600

    first migration, tables set up

---
+++ data/migrations/20190816094506_projects.js
@@ -0,0 +1,57 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('name', 255)
+            .notNullable()
+            .unique();
+        tbl.string('description', 500);
+        tbl.boolean('completed')
+            .defaultTo(false);
+    })
+    .createTable('resources', tbl => {
+        tbl.increments();
+        tbl.string('name', 255)
+            .notNullable()
+            .unique()
+        tbl.string('description', 500);
+    })
+    .createTable('project_resources', tbl => {
+        tbl.increments();
+        tbl.integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        tbl.integer('resource_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('description', 500)
+            .notNullable()
+            .unique();
+        tbl.string('notes', 500);
+        tbl.boolean('completed')
+            .defaultTo(false);
+        tbl.integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+};
